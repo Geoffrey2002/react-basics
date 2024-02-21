66f4271bf70152133dc3972bd25996f3787e79cc
Geoffrey <gmwangi3174@gmail.com>
2024-02-21 16:49:58 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import CoreConcepts from "./components/CoreConcepts";
+
+import { CORE_CONCEPTS } from "./data";
+import TabButton from "./components/TabButton";
+
+function App() {
+  useState();
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+  }
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts {...CORE_CONCEPTS[0]} />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
